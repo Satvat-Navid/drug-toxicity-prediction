829c2266077417d9513b390e6640e889531c5746
Satvat Navid <satvatnavid@gmail.com>
2026-03-31 07:13:12 +0530

    programming aise karo ki raat kali ho jai (key structure impact api remaining) :-(

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import sklearn
-import rdkit
-import fastapi
+from fastapi import FastAPI
+from pydantic import BaseModel
+from model import RandomForest
+import uvicorn
+
+app = FastAPI()
+
+model = RandomForest("tox21.csv")
+model.train()
+print(">>>>>Model feeded<<<<<<<")
+
+class SmileFormat(BaseModel):
+    smile : str
+
+@app.post("/pred")
+def pred(request: SmileFormat):
+    predict = model.predict_smiles(request.smile)
+
+    return predict
 
 
-df = pd.read_csv("tox21.csv")
+# app.mount("/", StaticFiles(directory="public", html=True), name="public")
 
-print(df.shape)
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=8088
+    )
